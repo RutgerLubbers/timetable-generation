e34a518d332a28f9614c35c57f4305dab607a694
Rutger Lubbers <RutgerLubbers@gmail.com>
2024-08-30 11:02:29 +0200

    Updated.

---
+++ backend/src/main/java/com/patrick/timetableappbackend/model/Constraint.java
@@ -0,0 +1,35 @@
+package com.patrick.timetableappbackend.model;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+public class ConstraintModel {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  // This is the constraint's name.
+  private String description;
+
+  @Enumerated(STRING)
+  private ConstraintWeight weight;
+}
--- backend/src/main/java/com/patrick/timetableappbackend/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.patrick.timetableappbackend.model;
-
-public enum Role {
-  ADMIN,
-  USER
-}
